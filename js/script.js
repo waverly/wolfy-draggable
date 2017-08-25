65200166905ee878a91ddcffb2941bbc41abf966
waverly <waverly.rose.mandel@gmail.com>
2017-08-24 18:46:34 -0700

    wolfy phase one

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.onload = function(e){
+
+  console.log('shit has loaded');
+
+  // $( function() {
+  //   $( ".canvas-monument" ).draggable().resizable({aspectRatio: true, handles: {'s': '#handle'}},);
+  //   // $(".canvas-monument img").resizable({aspectRatio: true});
+  // } );
+
+
+  const canvas = document.querySelector('.canvas-area');
+
+  // write fx that gets menu item src and appends new item
+  function monumentItemClick(){
+    const source = this.src;
+    const newEl = document.createElement('img');
+    newEl.src = source;
+    const child = document.createElement('div');
+    child.setAttribute('id', 'handle');
+    child.className += 'ui-resizable-handle';
+    child.className += " " + 'ui-resizable-s';
+
+    // create new div
+    const divEl = document.createElement('div');
+    divEl.className += "canvas-monument";
+    divEl.appendChild(child);
+    divEl.appendChild(newEl);
+    $(divEl).draggable().resizable({aspectRatio: true, handles: {'s': '#handle'}},);
+    canvas.appendChild(divEl);
+
+
+  }
+
+  // on click of one of the menu items, create a new element and append it to the main section of thebody
+  const monumentItem = Array.from(document.querySelectorAll('.monument-menu img'));
+  console.log(monumentItem);
+
+  // when you click on an item, run a fx that gets its src and console logs
+  const monumentSrc = monumentItem.forEach(item => item.addEventListener('click', monumentItemClick));
+
+
+}
